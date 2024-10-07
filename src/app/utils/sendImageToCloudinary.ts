1d060a31f236afc189bb8a982172911ed7435bdd
ferdousalam007 <iamferdousalam007@gmail.com>
2024-10-07 15:36:24 +0600

    add readme.md file

--- src/app/utils/sendImageToCloudinary.ts
+++ src/app/utils/sendImageToCloudinary.ts
@@ -9,35 +9,3 @@ cloudinary.config({
   api_secret: config.api_secret,
 });
 export default cloudinary
-// export const sendImageToCloudinary = async (
-//   imageName: string,
-//   path: string
-// ) => {
-//   return new Promise((resolve, reject) => {
-//     // upload image to cloudinary
-//     cloudinary.uploader.upload(
-//       path,
-//       {
-//         public_id: imageName,
-//       },
-//       function (error, result) {
-//         if (error) {
-//           reject(error);
-//         }
-//         resolve(result);
-//       }
-//     );
-//   });
-// };
-
-// const storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     cb(null, process.cwd() + "/uploads");
-//   },
-//   filename: function (req, file, cb) {
-//     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-//     cb(null, file.fieldname + "-" + uniqueSuffix);
-//   },
-// });
-
-// export const upload = multer({ storage: storage });
